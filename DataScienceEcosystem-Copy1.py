9890cf2ae2087a9fe9ee120b4a68e7b7501e53be
RipudamanSinghShahi <141746900+RipudamanSinghShahi@users.noreply.github.com>
2023-08-09 14:18:35 +0530

    Add files via upload

---
+++ DataScienceEcosystem-Copy1.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem 
+# 
+# 
+
+# In this notebook, Data Science Tools and Ecosystem are summarized
+
+# **Some of the popular Langues that Data Scientists use are :**
+#    
+#    1. Python
+#    2. R
+#    3. R Studio
+#    4. SQL
+
+# **Some of the commonly used librabies used by Data Scientists include:**
+#     
+#     1. NumPy
+#     2. Pandas
+#     3. Matplotlib
+#     4. Seaborn
+#     5. TensorFlow and PyTorch
+#     6. SciPy
+#     7. Jupyter
+# 
+
+#  | Data Science Tools |
+#  |--------------------|
+#  |Python              |
+#  |KNIME               |
+#  |MATLAB              |
+#  |Tensar Flow         |
+#  |NumPy               |
+#  |Jupyter             |
+#  |Apache Hadoop       |
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python:
+
+# This is a simple arithmetic expression to multiply then add integers
+
+# In[4]:
+
+
+(3*4)+5
+
+
+# This will convert 200 minutes to hours by diving by 60 and displaying result with upto two decimal places
+
+# In[5]:
+
+
+minutes = 200 
+hours = minutes / 60
+
+print(f"{minutes} minutes is equal to {hours:.2f} hours")
+
+
+# **Objectives**
+# 
+# In this Notebook, We covered the following key Takeaways:
+# 
+# -List of pupular programming languages commonly used by data scientests in thier day to day work.
+# 
+# -Overview of essential librabies frequestly employed in data science tasks.
+# 
+# -Demonstrated caculations using Jupyter Notebook for task such as data conversion(Minutes to Hours) and data calulation
+# 
+# Feel free to apply these concepts to you Data Science learning journey! Wishing you a great day ahead!
+
+# ## Author
+# 
+# Ripudaman Singh Shahi
+
+# In[ ]:
+
+
+
+
